73a45b80fe399ce96244f1e919c1b73c8d67f2dc
Herlandro <herlandro.hermogenes@gmail.com>
2025-04-08 12:43:50 +0100

    Added Project

---
+++ LLMPlayground/LLMPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  LLMPlaygroundApp.swift
+//  LLMPlayground
+//
+//  Created by Herlandro Hermogenes on 08/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct LLMPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
